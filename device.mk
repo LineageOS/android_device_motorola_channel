b5b16a37f505cccb03eac08f380e0dbecf38dd46
deadman96385 <seanhoyt963@gmail.com>
2020-04-17 06:56:13 -0500

    channel: Update fingerprint keylayout/idc
    Taken from android 10 release

--- device.mk
+++ device.mk
@@ -53,8 +53,8 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/uinput-egis.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-egis.kl \
-    $(LOCAL_PATH)/idc/uinput-egis.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-egis.idc
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc
 
 # Sensors
 PRODUCT_COPY_FILES += \
--- idc/uinput-fpc.idc
+++ idc/uinput-fpc.idc
@@ -1,6 +1,7 @@
 #
-# Egis Touch sensor driver
+# FPC1020 Touch sensor driver
 #
+# Copyright (c) 2013,2014 Fingerprint Cards AB <tech@fingerprints.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License Version 2
@@ -8,6 +9,7 @@
 #
 
 device.internal = 1
-keyboard.layout = uinput-egis
+
+keyboard.layout = uinput-fpc
 keyboard.builtIn = 1
 keyboard.orientationAware = 1
