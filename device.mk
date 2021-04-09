df12c3f0e199f7439582e461dc6bd7ce1cdf907c
Kevin F. Haggerty <haggertk@lineageos.org>
2021-04-09 08:18:37 -0600

    channel: Don't let builds complete without vendor tree
    * There is zero reason to ever build without blobs. We've even seen
      this situation with official builds from our servers. It's always
      better for a build to fail than it is for it to produce something
      that has no chance at working.
    
    Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # Vendor blobs
-$(call inherit-product-if-exists, vendor/motorola/channel/channel-vendor.mk)
+$(call inherit-product, vendor/motorola/channel/channel-vendor.mk)
 
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
