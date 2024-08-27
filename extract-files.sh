7b3ad49530e97be485abb228d815b0154d147b84
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:51:30 +0300

    channel: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -10,9 +10,19 @@ function blob_fixup() {
     case "${1}" in
         # libutils-v32
         vendor/lib/sensors.ssc.so | vendor/lib64/sensors.ssc.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -26,5 +36,6 @@ set -e
 export DEVICE=channel
 export DEVICE_COMMON=sdm632-common
 export VENDOR=motorola
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
