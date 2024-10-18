aa9027841cc56ccf6db1547822fd41152757cd36
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    channel: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_channel.mk
+++ lineage_channel.mk
@@ -6,7 +6,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-BUILD_FINGERPRINT := "motorola/channel_retail/channel:10/QPYS30.85-23-8-2/c00f57:user/release-keys"
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := channel
 PRODUCT_MANUFACTURER := motorola
@@ -14,5 +13,6 @@ PRODUCT_MODEL := moto g(7) play
 PRODUCT_NAME := lineage_channel
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=channel \
-    PRIVATE_BUILD_DESC="channel_retail-user 10 QPYS30.85-23-8-2 c00f57 release-keys"
+    BuildDesc="channel_retail-user 10 QPYS30.85-23-8-2 c00f57 release-keys"
+    BuildFingerprint=motorola/channel_retail/channel:10/QPYS30.85-23-8-2/c00f57:user/release-keys \
+    DeviceName=channel
